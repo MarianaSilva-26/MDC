d27662ecd3326fe99303dc7407759700da515d64
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-11-24 03:02:22 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A; 
+            int B;
+            int mdc;
+            bool fim = true;
+
+            Console.WriteLine("Máximo Divisor Comum");
+            Console.Write("1° número: "); 
+            A = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("2° número: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+        while (fim)
+        {
+        if (A % B ==0)
+        {
+            Console.WriteLine($"MDC (a, b) = {A}");  
+            fim = false; 
+        }
+        else
+        {
+            mdc = A % B; 
+            A = B; 
+            B = mdc; 
+        }
+        }
+        }
+    }
+}
